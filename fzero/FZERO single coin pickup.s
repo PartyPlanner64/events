0e096932dd76681481add8d23eced553cdac70b4
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-01-27 20:56:01 -0600

    Simplify coin pickup event

--- fzero/FZERO single coin pickup.s
+++ fzero/FZERO single coin pickup.s
@@ -6,31 +6,21 @@ ADDIU SP SP -8
 SW RA 4(SP)
 SW S0 0(SP)
 
-JAL guRandom ; Generates random 32-bit int
-NOP
-; Random int was returned in V0
+JAL RNGPercentChance
+ADDIU A0 R0 50 ; 50% chance
+; True/false bit was returned in V0
 
-; 50% chance of either outcome (25%?)
-.definelabel halfOf32Bit,0x1FFFFFFF
-LUI T0 hi(halfOf32Bit)
-ADDIU T0 T0 lo(halfOf32Bit)
-SLT S0 V0 T0 ; is V0 < 0x1FFFFFFF ?
-NOP
-
-BEQ S0 R0 exit
+BEQ V0 R0 exit
 NOP
 
 JAL GetCurrentPlayerIndex
 NOP
-
 ADDIU A0 V0 0
-ADDIU A1 A1 1 ; +1 coin
 JAL AdjustPlayerCoins
-NOP
+ADDIU A1 R0 1 ; +1 coin
 
-ADDIU A0 R0 0x106 ; Coin add sound
 JAL PlaySound
-NOP
+ADDIU A0 R0 0x106 ; Coin add sound
 
 exit:
 LW RA 4(SP)
