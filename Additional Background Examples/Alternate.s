163a4874101ac8bb89108e87a368bc62e61704ce
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-06-02 17:53:30 -0500

    Add additional bg example

---
+++ Additional Background Examples/Alternate.s
@@ -0,0 +1,28 @@
+; This uses the main background on odd turns
+; and the additional background on event turns.
+ADDIU SP SP -4
+SW RA 0(SP)
+
+; Load the current turn number into T0
+LUI T0 hi(current_turn)
+ADDIU T0 T0 lo(current_turn)
+LBU T0 0(T0)
+
+; T0 = T0 & 1
+; In other words, T0 is now true if the current turn is odd.
+ANDI T0 T0 1
+
+BEQZ T0 usealternate
+NOP
+
+LI V0 DEFAULT_BG
+J exit
+NOP
+
+usealternate:
+LI V0 ADDITIONAL_BG_1
+
+exit:
+LW RA 0(SP)
+JR RA
+ADDIU SP SP 4
---
+++ Additional Background Examples/README.md
@@ -0,0 +1,4 @@
+Additional Background Examples
+==============================
+
+The files in this folder are examples of [additional background](https://github.com/PartyPlanner64/PartyPlanner64/wiki/Additional-Backgrounds) codes, not full events. This kind of code cannot be imported from a file; you can copy and paste these examples if you'd like to use them.
