d299c3a700799b21375f8afd1c87b04871946dd1
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-11-25 15:53:39 -0600

    Add indices

--- C/Model Viewer.c
+++ C/Model Viewer.c
@@ -93,3 +93,115 @@ void main() {
         func_8003E694(model_obj);
     }
 }
+
+// 0: Mario (low-res)
+// 1: Luigi
+// 2: Peach
+// 3: Yoshi
+// 4: Wario
+// 5: DK
+// 6: Bowser
+// 7: Toad
+// 8: Koopa Troopa
+// 9: Boo
+// 10: Whomp
+// 11: Goomba
+// 12: Piranha Plant
+// 13: Blue Thwomp
+// 14: Black Bob-omb
+// 15: Red Bob-omb
+// 16: Mecha Fly Guy
+// 17: Pisudon (Robot that switches gates in Luigi's Engine Room)
+// 18: Cloud with face (Mario Rainbow Castle)
+// 19: Lakitu
+// 20: Top of castle from Mario Rainbow Castle
+// 21: Black Star
+// 22: Pipe
+// 23: Mario (high-res)
+// 24: Luigi
+// 25: Peach
+// 26: Yoshi
+// 27: Wario
+// 28: DK
+// 29: Mario Pin (from cake)
+// 30: Luigi pin
+// 31: Peach pin
+// 32: Yoshi pin
+// 33: Wario pin
+// 34: DK pin
+// 35: Piranha Plant 2D
+// 36: Red Direction Arrow
+// 37: Star
+// 38: Boulder
+// 39: Golden Banana
+// 40: (crash)
+// 41: 20 coin gate from DK Jungle Adventure
+// 42: Box holding Golden Bananas
+// 43: Small Wood Crate
+// 44: Gray thing?
+// 45: Black/white thing?
+// 46: Black Bob-omb Cannon
+// 47: Bowser Cannon
+// 48: Black Bob-omb flag
+// 49: Red Bob-omb flag
+// 50: Black/Red Bob-omb flag
+// 51: Blue spiked gate (Luigi's Engine Room)
+// 52: Engine part (Luigi's Engine Room)
+// 53: Big skull thing from Bowser's Magma Mountain
+// 54: Direction skull face from Bowser's Magma Mountain
+// 55: Direction skull face from Bowser's Magma Mountain
+// 56: Black ball
+// 57: Koopa Troopa (low-res)
+// 58: Toad (2D)
+// 59: Bowser (low-res)
+// 60: Machine that bowser uses in Luigi's Magma Mountain
+// 61: Coin
+// 62: Golden Banana
+// 63: Box holding Golden Bananas
+// 64: Star
+// 65: Boulder (from sea?)
+// 66: Sky boat
+// 67: Basic cannon
+// 68: Basic Bowser cannon
+// 69: Mario Head
+// 70: Luigi Head
+// 71: Peach Head
+// 72: Yoshi Head
+// 73: Wario Head
+// 74: DK Head
+// 75: ?
+// 76: Green Warp Effect
+// 77: White shine Effect
+// 78: White circle Effect
+// 79: Magma Effect
+// 80: Very low poly Mario
+// 81: Very low poly Luigi
+// 82: Very low poly Peach
+// 83: Very low poly Yoshi
+// 84: Very low poly Wario
+// 85: Very low poly DK
+// 86: Flames
+// 87: White cloudy Effect
+// 88: Mushroom
+// 89: Coin bank (purple, two arrows)
+// 90: Coin bank (golden)
+// 91: Coin bank (green with star)
+// 92: Big fish
+// 93: Toad
+// 94: Mario (stick model)
+// 95: Luigi (stick model)
+// 96: Peach (stick model)
+// 97: Yoshi (stick model)
+// 98: Wario (stick model)
+// 99: DK (stick model)
+// 100: Rainbow warp Effect
+// 101: Pink Yoshi
+// 102: Blue Yoshi
+// 103: White cloud
+// 104: Star?
+// 105: Star?
+// 106: Hovering Boo
+// 107: White shine Effect
+// 108: Bowser (high res)
+// 109: Boo (high res)
+// 110: (crash)
