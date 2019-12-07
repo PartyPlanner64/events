9d589c7e99793207ef03168644f62cdc3f6bd76a
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-12-07 17:15:25 -0600

    Fix typo

--- Additional Background Examples/Alternate.c
+++ Additional Background Examples/Alternate.c
@@ -3,7 +3,7 @@ extern unsigned char current_turn;
 // This uses the main background on odd turns
 // and the additional background on even turns.
 int PickBackground() {
-    if (current_turn & 1 == 0) {
+    if ((current_turn & 1) == 0) {
       return ADDITIONAL_BG_1;
     }
     else {
