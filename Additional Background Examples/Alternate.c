65b5d57ac33bae7b6b8a56a26da4276269272788
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-12-07 16:47:43 -0600

    Add example of C additional bg code

---
+++ Additional Background Examples/Alternate.c
@@ -0,0 +1,12 @@
+extern unsigned char current_turn;
+
+// This uses the main background on odd turns
+// and the additional background on event turns.
+int PickBackground() {
+    if (current_turn & 1 == 0) {
+      return ADDITIONAL_BG_1;
+    }
+    else {
+      return DEFAULT_BG;
+    }
+}
