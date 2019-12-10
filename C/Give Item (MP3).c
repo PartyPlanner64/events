6107010b3f7410acf0b67bb88ad093c4b38be417
PartyPlanner64 <PartyPlanner64@users.noreply.github.com>
2019-12-10 09:35:44 -0600

    Fix struct. Add item enum.

--- C/Give Item (MP3).c
+++ C/Give Item (MP3).c
@@ -11,6 +11,29 @@ typedef unsigned int           u32;
 typedef float  f32;
 typedef double f64;
 
+enum item {
+    ITEM_NONE = 0xFF,
+    ITEM_MUSHROOM = 0x00,
+    ITEM_SKELETON_KEY = 0x01,
+    ITEM_POISON_MUSHROOM = 0x02,
+    ITEM_REVERSE_MUSHROOM = 0x03,
+    ITEM_CELLULAR_SHOPPER = 0x04,
+    ITEM_WARP_BLOCK = 0x05,
+    ITEM_PLUNDER_CHEST = 0x06,
+    ITEM_BOWSER_PHONE = 0x07,
+    ITEM_DUELING_GLOVE = 0x08,
+    ITEM_LUCKY_LAMP = 0x09,
+    ITEM_GOLDEN_MUSHROOM = 0x0A,
+    ITEM_BOO_BELL = 0x0B,
+    ITEM_BOO_REPELLANT = 0x0C,
+    ITEM_BOWSER_SUIT = 0x0D,
+    ITEM_MAGIC_LAMP = 0x0E,
+    ITEM_KOOPA_CARD = 0x0F,
+    ITEM_BARTER_BOX = 0x10,
+    ITEM_LUCKY_COIN = 0x11,
+    ITEM_WACKY_WATCH = 0x12
+};
+
 struct player {
     s8 unk0;
     s8 cpu_difficulty;
@@ -42,27 +65,26 @@ struct player {
     u8 reverse_space_index; // 22
 
     u8 flags2; // 23
-    u8 items[3]; // 24
+    enum item items[3]; // 24
     u8 bowser_suit_flag; // 27
     u8 turn_color_status; // 28
 
     s8 pad1[7]; // 29 - 35
 
-    void *obj; // struct object *
-    s8 pad2[1]; // 40
-    s16 minigame_star; // 41
+    void *obj; // 36 // struct object *
+    s16 minigame_star; // 40
     s16 coin_star; // 42
-    s8 happening_space_count;
+    s8 happening_space_count; // 44
     s8 red_space_count;
     s8 blue_space_count;
-    s8 chance_space_count; // 46
-    s8 bowser_space_count;
+    s8 chance_space_count;
+    s8 bowser_space_count; // 48
     s8 battle_space_count;
     s8 item_space_count;
-    s8 bank_space_count; // 50
-    s8 game_guy_space_count; // 51
+    s8 bank_space_count;
+    s8 game_guy_space_count; // 52
 
-    s8 pad3[5];
+    // s8 pad2[3];
 }; // sizeof == 56
 
 extern s16 GetCurrentPlayerIndex();
@@ -74,26 +96,6 @@ void main() {
     s32 open_index = PlayerHasEmptyItemSlot(cur_player_index);
     if (open_index != -1) {
       struct player *player = GetPlayerStruct(cur_player_index);
-      player->items[open_index] = 0x0A; // Golden Mushroom
+      player->items[open_index] = ITEM_GOLDEN_MUSHROOM;
     }
 }
-
-// 0x00 = Mushroom
-// 0x01 = Skeleton Key
-// 0x02 = Poison Mushroom
-// 0x03 = Reverse Mushroom
-// 0x04 = Cellular Shopper
-// 0x05 = Warp Block
-// 0x06 = Plunder Chest
-// 0x07 = Bowser Phone
-// 0x08 = Dueling Glove
-// 0x09 = Lucky Lamp
-// 0x0A = Golden Mushroom
-// 0x0B = Boo Bell
-// 0x0C = Boo Repellant
-// 0x0D = Bowser Suit
-// 0x0E = Magic Lamp
-// 0x0F = Koopa Card
-// 0x10 = Barter Box
-// 0x11 = Lucky Coin
-// 0x12 = Wacky Watch
